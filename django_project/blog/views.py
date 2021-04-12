be8a3141a5f8b2b7238c075aa8980f5dbee49de6
Vincent <vincent.tan.1998@gmail.com>
2021-04-12 00:16:57 -0400

    add tags to posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -45,7 +45,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'content', 'tags']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
